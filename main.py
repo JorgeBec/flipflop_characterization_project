258abf3c2ab2a1db576cf3f5fd7203ab7740d555
Jesus Michael <Villa Ortiz>
2025-05-14 19:51:45 -0700

    Funcion nueva - generador

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
+<<<<<<< Updated upstream
 print("Hello World")
 print("test 14/05/25")
 print("hola")
 print('greta')
+=======
+
+import pyvisa
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Inicializa la conexión con el Resource Manager
+rm = pyvisa.ResourceManager()
+
+# Lista todos los recursos disponibles
+recursos = rm.list_resources()
+print("Dispositivos encontrados:")
+for i, recurso in enumerate(recursos):
+    print(f"{i+1}: {recurso}")
+
+# Intenta identificar cada dispositivo
+print("\nIdentificando cada dispositivo con *IDN?...*")
+for recurso in recursos:
+    try:
+        dispositivo = rm.open_resource(recurso)
+        idn = dispositivo.query("*IDN?")
+        print(f"{recurso} -> {idn.strip()}")
+        dispositivo.close()
+    except Exception as e:
+        print(f"{recurso} -> No responde a *IDN? ({e})")
+
+
+>>>>>>> Stashed changes
