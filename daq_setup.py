3ecefbc62a7846fcf1ee3997085149cc24535b92
Becerra <jorge.becerra@uabc.edu.mx>
2025-05-22 09:36:17 -0700

    nose

--- daq_setup.py
+++ daq_setup.py
@@ -1,4 +1,5 @@
 import nidaqmx
+import time
 
 def initialize_daq_outputs_zero(device_name="Dev1"):
     with nidaqmx.Task() as task:
@@ -9,6 +10,12 @@ def initialize_daq_outputs_zero(device_name="Dev1"):
         # Write 0 volts to both channels
         task.write([0.0, 0.0])
         print(f"Set {device_name}/ao0 and {device_name}/ao1 outputs to 0 V.")
+
+        task.do_channels.add_do_chan("Dev1/port1/line2") #clear
+        task.write(True)  # Write voltages to ao0 and ao1 simultaneously
+        time.sleep(0.1)
+        task.write(False)
+        
 
 if __name__ == "__main__":
     initialize_daq_outputs_zero()
