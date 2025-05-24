23aa450ac297726ae78462e7894d44807ad8e684
Becerra <jorge.becerra@uabc.edu.mx>
2025-05-24 14:56:17 -0700

    tdelay completed

--- td_crossing_time.py
+++ td_crossing_time.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def find_crossing_time(time1, voltages1, time2, voltages2):
+def find_crossing_time(time1, voltages1, time2, voltages2,type_test):
     """
     Find the time difference between the first threshold crossings (50% amplitude) of two waveforms.
     """
@@ -10,23 +10,37 @@ def find_crossing_time(time1, voltages1, time2, voltages2):
     threshold2 = (np.max(voltages2) / 2)
     print(f"Threshold ch2: {threshold2:.3f} V")
 
-    cross1 = None
-    for i in range(len(voltages1)):
-        if voltages1[i] >= threshold1:
-            cross1 = time1[i]
-            print(cross1)
-            break  # Stop at first crossing
+    if type_test == "1": # High to Low
+        cross1 = None
+        for i in range(len(voltages1)):
+            if voltages1[i] <= threshold1:
+                cross1 = time1[i]
+                print(cross1)
+                break  # Stop at first crossing
 
-    cross2 = None
-    for i in range(len(voltages2)):
-        if voltages2[i] >= threshold2:
-            cross2 = time2[i]
-            print(cross2)
-            break  # Stop at first crossing
+        cross2 = None
+        for i in range(len(voltages2)):
+            if voltages2[i] <= threshold2:
+                cross2 = time2[i]
+                print(cross2)
+                break  # Stop at first crossing
+        
+    elif type_test == "2": # Low to High
+        cross1 = None
+        for i in range(len(voltages1)):
+            if voltages1[i] <= threshold1:
+                cross1 = time1[i]
+                print(cross1)
+                break  # Stop at first crossing
 
-    if cross1 is None or cross2 is None:
-        print("Warning: One of the channels did not cross its threshold.")
-        return None
+        cross2 = None
+        for i in range(len(voltages2)):
+            if voltages2[i] >= threshold2:
+                cross2 = time2[i]
+                print(cross2)
+                break  # Stop at first crossing
+
+
 
     crossing_time = cross2 - cross1
     print(f"Propagation delay: {crossing_time * 1e9:.2f} ns")
