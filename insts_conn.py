f15f9136e63ba9a261466946b663e17fcaa7ff86
Dylan Aguilar <aguilar.dylan@uabc.edu.mx>
2025-05-23 20:16:22 -0700

    Folder Cleaned

--- insts_conn.py
+++
@@ -1,2 +0,0 @@
-print("This file is for connecting with the instruments")
-print("test 4")
--- main_becerra.py
+++
@@ -1,8 +0,0 @@
-from dev_setup import find_instruments
-
-osc, dmm, psu, fg = find_instruments()
-
-
-osc.write("SELECT:CH1 ON")
-osc.write("SELECT:CH2 ON")
-print("Oscilloscope ready.")
